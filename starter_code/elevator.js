4d1abe5a2117f2f971b9e3b3d1bbc2d94c3be7dd
Clementina <mataclemy@gmail.com>
2017-11-06 17:53:15 +0100

    iteration 1, 2

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,51 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = "up";
+    this.interval = 0;
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.interval = setInterval(this.update, 1000);
+  }
+
+  stop() {
+    clearInterval(this.interval);
+  }
+
+  update() {
+    this.log();
+  }
+
+  _passengersEnter() {}
+  _passengersLeave() {}
+
+  floorUp() {
+    if (this.floor < this.MAXFLOOR) {
+      this.floor++;
+    } else {
+      console.log("You are in the highest floor");
+    }
+  }
+
+  floorDown() {
+    if (this.floor >= 0) {
+      this.floor--;
+    } else {
+      console.log("You are in floor 0");
+    }
+  }
+
+  call() {}
+
+  log() {
+    console.log("direction: " + this.direction, "floor: " + this.floor);
+  }
 }
+
+
+
 
 module.exports = Elevator;
